971db65c1153b2bb866a1fee89dd32e437ea5598
Brijendra-Singh2003 <brijendra0369@gmail.com>
2024-12-12 19:24:22 +0530

    added player animations

--- src/game/prefabs/Player.ts
+++ src/game/prefabs/Player.ts
@@ -1,5 +1,6 @@
 export default class Player extends Phaser.Physics.Arcade.Sprite {
     playerName?: Phaser.GameObjects.Text
+    direction = "down";
 
     constructor(scene: Phaser.Scene, x: number, y: number, texture: string | Phaser.Textures.Texture, name: string) {
         super(scene, x, y, texture);
@@ -11,6 +12,10 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
         // Set properties like bounce
         this.setBounce(0.2);
         this.setCollideWorldBounds(true);
+        
+        this.setScale(1.5);
+        this.body?.setSize(16, 16);
+        this.body?.setOffset(16, 28);
 
         // Add player name text
         this.playerName = scene.add.text(this.x, this.y - 20, name, {
@@ -22,8 +27,19 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
         scene.events.on('update', this.update, this);
     }
 
+    setSpeed(vx: number, vy: number) {
+        this.setVelocity(vx, vy);
+        
+        const state = (vx || vy) ? "walk" : "idle";
+
+        if(vx) this.direction = (vx > 0) ? "right" : "left";
+        else if(vy) this.direction = (vy > 0) ? "down" : "up";
+
+        this.anims.play(state + '_' + this.direction, true);
+    }
+
     // Override update to keep the text above the player
-    update(...args: any[]): void {
+    update() {
         this.playerName?.setPosition(this.x, this.y - 24);
     }
 
