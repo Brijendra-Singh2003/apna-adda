5de8664cf5bd986fafa41cceef8faffef6ad7a47
Brijendra-Singh2003 <brijendra0369@gmail.com>
2024-12-26 19:10:24 +0530

    fixed no space in chat bug

--- src/components/ChatSection.tsx
+++ src/components/ChatSection.tsx
@@ -95,6 +95,7 @@ const ChatSection: React.FC<ChatSectionProps> = ({
                   placeholder="Type your message..."
                   value={newMessage}
                   onChange={(e) => setNewMessage(e.target.value)}
+                  onKeyDown={(e) => e.stopPropagation()}
                   className="flex-1 px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
                 <button
